6800539cff55e996eb0b73c355f70924abbb0b76
詹迟晶 <james.zhan@bytedance.com>
2018-05-15 14:20:02 +0800

    Initial Commit

---
+++ AssetsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssetsDemo
+//
+//  Created by Whirlwind on 2018/5/15.
+//  Copyright © 2018年 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
